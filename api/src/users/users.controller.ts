94148dd5a569b7fb502215d0d511d84db1f6f1b0
janets-raha <jeannette.cecora@gmail.com>
2020-10-06 21:55:12 +0200

    add controller

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -12,11 +12,11 @@ export class UsersController {
         @Body('name') name: string,
         @Body('email') email: string,
         @Body('password') password: string,
-        @Body('favorites') favorites: Mongoose.Schema.Types.ObjectId,
+        @Body('favorites') favorites [],
         @Body('score') score: string,
         @Body('role') role: string,
     ) {
-        const newUser = await this.userService.createUser(
+        const newUser = await this.usersService.insertUser(
           name,
           email,
           password,
@@ -28,8 +28,8 @@ export class UsersController {
     }
 
     @Get()
-    async showAllUsers() {
-        const allUsers = await this.userService.showAll();
-        return allUsers;
+    async getAllUsers() {
+        const users = await this.usersService.getUsers();
+        return users;
     }
 }
