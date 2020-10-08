6afbd4336bca08dd4a864bc6b41c21103ff2c4a3
Nicolas Coulot <nicolas.coulot@epitech.eu>
2020-10-08 19:37:35 +0200

    NEW adminQuiz list display OK

--- app/src/apis/AdminQuiz.js
+++ app/src/apis/AdminQuiz.js
@@ -1,41 +1,41 @@
-import Api from "./Api";
+import Api from './Api';
 export default {
   getCategories() {
-    return Api().get("/category/");
+    return Api().get('/category/');
   },
   addCategory(payload) {
-    return Api().post("/category/", payload.data);
+    return Api().post('/category/', payload.data);
   },
   getQuizzes() {
-    return Api().get("/quiz");
+    return Api().get('/quizz');
   },
   addQuiz(payload) {
-    return Api().post("/quizz", payload.data);
+    return Api().post('/quizz', payload.data);
   },
   updateQuiz(payload) {
-    return Api().patch("/quiz/" + payload.quizId, payload.data);
+    return Api().patch('/quiz/' + payload.quizId, payload.data);
   },
   deleteQuiz(quizId) {
-    return Api().delete("/quiz/" + quizId);
+    return Api().delete('/quiz/' + quizId);
   },
 
   getQuestions(quizId) {
-    return Api().get("/question/" + quizId);
+    return Api().get('/question/' + quizId);
   },
 
   addQuestions(payload) {
-    return Api().post("/question", payload.data);
+    return Api().post('/question', payload.data);
   },
 
   updateQuestions(payload) {
-    return Api().put("/question", payload.data);
+    return Api().put('/question', payload.data);
   },
 
   updateQuestion(payload) {
-    return Api().patch("/question/" + payload.questionId, payload.data);
+    return Api().patch('/question/' + payload.questionId, payload.data);
   },
 
   deleteQuestions(questionId) {
-    return Api().delete("/question/" + questionId);
+    return Api().delete('/question/' + questionId);
   },
 };
--- app/src/components/AdminQuiz.vue
+++ app/src/components/AdminQuiz.vue
@@ -1,10 +1,71 @@
 <template>
   <b-container class="py-3">
     <b-button to="/newquiz" variant="success">Ajouter un Quiz</b-button>
+
+    <!--Labels -->
+    <b-row no-gutter id="labels" class="text-center" align-v="center">
+      <b-col>
+        <strong> Titre </strong>
+      </b-col>
+      <b-col>
+        <strong> Techno </strong>
+      </b-col>
+      <b-col>
+        <strong> Difficulté </strong>
+      </b-col>
+      <b-col> </b-col>
+      <b-col> </b-col>
+    </b-row>
+
+    <!--Cards -->
+    <div v-for="(quiz, idx) in quizz" :key="idx">
+      <b-row no-gutter id="quizCard" class="text-center" align-v="center">
+        <b-col>
+          <strong>
+            {{ quiz.name }}
+          </strong>
+        </b-col>
+        <b-col>
+          {{ quiz.category.name }}
+        </b-col>
+        <b-col v-if="quiz.difficulty === 'Facile'" class="h1">
+          <b-icon icon="reception1" variant="success" class="mr-1"></b-icon>
+        </b-col>
+        <b-col v-if="quiz.difficulty === 'Moyen'" class="h1">
+          <b-icon icon="reception2" variant="warning" class="mr-1"></b-icon>
+        </b-col>
+        <b-col v-if="quiz.difficulty === 'Difficile'" class="h1">
+          <b-icon icon="reception3" variant="danger" class="mr-1"></b-icon>
+        </b-col>
+        <b-col>
+          <b-button variant="warning" class="btn-block">
+            <b-icon icon="pencil" variant="dark" class="mr-1"></b-icon>
+            Modifier
+          </b-button>
+        </b-col>
+        <b-col v-if="quiz.is_published === true">
+          <b-button variant="success" class="btn-block">
+            <b-icon icon="box-arrow-up" variant="light" class="mr-1"></b-icon>
+            Publier
+          </b-button>
+        </b-col>
+        <b-col v-else>
+          <b-button variant="outline-success" class="btn-block">
+            <b-icon
+              icon="box-arrow-in-down"
+              variant="success"
+              class="mr-1"
+            ></b-icon>
+            Dépublier
+          </b-button>
+        </b-col>
+      </b-row>
+    </div>
   </b-container>
 </template>
 
 <script>
+import AdminQuiz from "../apis/AdminQuiz";
 export default {
   data() {
     return {
@@ -12,6 +73,7 @@ export default {
       isAdmin: false,
       user: null,
       notifCount: 0,
+      quizz: [],
     };
   },
   xupdated() {
@@ -22,19 +84,10 @@ export default {
       });
     }
   },
-  xmounted() {
-    this.$root.$on("login", () => {
-      this.isLoggedIn = true;
+  mounted() {
+    AdminQuiz.getQuizzes().then((response) => {
+      this.quizz = response.data;
     });
-
-    this.isLoggedIn = !!localStorage.getItem("token");
-
-    if (this.isLoggedIn) {
-      User.auth().then((response) => {
-        this.user = response.data;
-        this.isAdmin = this.user.role == "admin";
-      });
-    }
   },
   methods: {
     logout() {
@@ -47,3 +100,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#quizCard {
+  margin-top: 10px;
+  border: 1px solid #498824;
+  border-radius: 10px;
+  vertical-align: middle;
+}
+
+#labels {
+  margin-top: 20px;
+}
+</style>
