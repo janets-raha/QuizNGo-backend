ffc82c8fca0ee383e24a6e58143b617f1b6ba479
janets-raha <jeannette.cecora@gmail.com>
2020-10-08 16:50:03 +0200

    implement default strategy

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { LocalStrategy } from './local.strategy';
 @Module({
   imports: [
     UsersModule, 
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '24h' },
