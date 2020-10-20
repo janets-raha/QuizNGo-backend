c9b34652e2c81a447bca0ddf2ce52ec72b8d54b0
Nicolas Coulot <nicolas.coulot@epitech.eu>
2020-10-20 12:01:09 +0200

    CLEAN-DELETE swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,24 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { AppService } from './app.service';
-import { ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger'
+import { Controller, Get, Request, Post, UseGuards } from "@nestjs/common";
+import { LocalAuthGuard } from "./auth/local-auth.guard";
+import { AuthService } from "./auth/auth.service";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly authService: AuthService) { }
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
-  @ApiOkResponse({ description: 'User login' })
-  @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
   @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
+  @Post("auth/login")
   async login(@Request() req) {
     return this.authService.login(req.body);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('profile')
+  @Get("profile")
   getProfile(@Request() req) {
     return req.user;
   }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
-  const options = new DocumentBuilder()
-    .setTitle('Qwiz & Go API')
-    .setDescription('Description of Qwiz & Go API')
-    .setVersion('1.0')
-    .addTag('Quiz')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document)
-
   await app.listen(3000);
 }
 bootstrap();
