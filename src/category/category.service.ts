992bb1c4b6d29e09df13b40bde32eb23830390d7
Nicolas Coulot <nicolas.coulot@epitech.eu>
2020-10-22 14:47:42 +0200

    UPDATE delete category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,14 +1,20 @@
 import * as Mongoose from "mongoose";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Category } from "./category.model";
+import { Quizz } from "src/quizz/quizz.model";
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectModel("Category") private readonly categoryModel: Model<Category>,
-  ) { }
+    @InjectModel("Quizz") private readonly quizModel: Model<Quizz>,
+  ) {}
 
   async createCategory(name: string) {
     const newCategory = new this.categoryModel({ name });
@@ -35,7 +41,7 @@ export class CategoryService {
   async update(catId: Mongoose.Schema.Types.ObjectId, name: string) {
     const category = await this.categoryModel.findOne({ _id: catId }).exec();
     if (!category) {
-      throw new NotFoundException("Category not found");
+      throw new NotFoundException("Categorie non trouvée");
     } else {
       category.name = name;
       category.save();
@@ -44,11 +50,20 @@ export class CategoryService {
   }
 
   async delete(catId: Mongoose.Schema.Types.ObjectId) {
-    const category = await this.categoryModel.deleteOne({ _id: catId }).exec();
-    if (category.deletedCount === 0) {
-      throw new NotFoundException("Category not found");
+    const checkCatInQuiz = await this.quizModel.findOne({ category: catId });
+    if (!checkCatInQuiz) {
+      const category = await this.categoryModel
+        .deleteOne({ _id: catId })
+        .exec();
+      if (category.deletedCount === 0) {
+        throw new NotFoundException("Categorie non trouvée");
+      } else {
+        return "Categorie supprimée avec succès";
+      }
     } else {
-      return "Category successfully deleted";
+      throw new NotAcceptableException(
+        "Vous ne pouvez pas supprimer une catégorie utilisée dans un quiz !",
+      );
     }
   }
 }
